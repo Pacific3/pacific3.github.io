6958210539a8ead74ac3c4fa70fd1c06ba3f02bd
Oscar Swanros <oscar@swanros.com>
2017-05-19 18:44:07 -0500

    FAcebook Pixel and Google Analytics

--- index.php
+++ index.php
@@ -24,6 +24,31 @@
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="css/animate.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+     <!-- Facebook Pixel Code -->
+     <script>
+     !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+     n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+     n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+     t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+     document,'script','https://connect.facebook.net/en_US/fbevents.js');
+     fbq('init', '293713390995939'); // Insert your pixel ID here.
+     fbq('track', 'PageView');
+     </script>
+     <noscript><img height="1" width="1" style="display:none"
+     src="https://www.facebook.com/tr?id=293713390995939&ev=PageView&noscript=1"
+     /></noscript>
+     <!-- DO NOT MODIFY -->
+     <!-- End Facebook Pixel Code -->
+     <script>
+       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+       ga('create', 'UA-99302821-1', 'auto');
+       ga('send', 'pageview');
+
+     </script>
 </head>
 <body>
 	<section class="banner" role="banner">
